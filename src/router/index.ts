2d65ca845d4e9e77cf2c25771bb74e024418fc7e
Mart <mart_lepanen@hotmail.com>
2025-03-09 10:48:48 +0200

    Start adding TicTacToe view

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/tictactoe',
+      name: 'tictactoe',
+      component: () => import('../views/TicTacToeView.vue'),
+    },
   ],
 })
 
---
+++ src/views/TicTacToeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="view">
+    <h1>This is the game view</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .vue {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
